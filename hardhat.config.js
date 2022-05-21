98b83aede080d33141b25a4daf6a445f48ab0767
iamoracle <officialbilas@gmail.com>
2022-05-21 14:12:46 +0100

    UPDATE: switched to privatekey

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,10 +21,7 @@ module.exports = {
   networks: {
     alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
-      accounts: {
-        mnemonic: process.env.MNEMONIC,
-        path: "m/44'/52752'/0'/0"
-      },
+      accounts: [ process.env.SECRET_KEY ]
     }
   },
   solidity: "0.8.4",
